ac70cfd66197ba0af2dc53f54f57009e8b56e6f2
Codingislikelearninganewlanguage <121121504+Codingislikelearninganewlanguage@users.noreply.github.com>
2022-12-20 17:12:43 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+// See https://aka.ms/new-console-template for more information
+// Open Source Caculator made by codingislikelearninganewlanguage(ahmetguzel2010@outlook.com)
+Console.WriteLine("First number please");
+var firstnum = Console.ReadLine();
+Console.WriteLine("Second number please");
+var secondnum = Console.ReadLine();
+Console.WriteLine("Method 1:Addition 2:Subtraction 3:Multiplication");
+var method = Console.ReadLine();
+var firstnumparsed = int.Parse(firstnum);
+var secondnumparsed = int.Parse(secondnum);
+if (method == "1") 
+{
+    Console.WriteLine(firstnumparsed + secondnumparsed);
+}
+if(method == "2")
+{
+    Console.WriteLine(firstnumparsed - secondnumparsed);
+}
+if (method == "3")
+{
+    Console.WriteLine(firstnumparsed * secondnumparsed);
+}
